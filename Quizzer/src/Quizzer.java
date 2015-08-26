28b18abee35d6253a7570b1780c27f3e9b5d86e3
ShadowOnyx <Jonatan.Oliveira@MU-C1-18.mullen.pvt.k12.co.us>
2015-08-26 12:41:31 -0600

    Finished V1

--- Quizzer/src/Quizzer.java
+++ Quizzer/src/Quizzer.java
@@ -1,22 +1,26 @@
 import java.util.Scanner;
 public class Quizzer
 	{
+		static int answerCounter;
 		static int counter;
+		static int numberQ;
+		static int counter1;
 		public static void main(String[] args)
 			{
-			System.out.println("Welcome to Quizzer 1.0, how many questions will you do today?");	
+			System.out.println("Welcome to Quizzer 1.5, how many questions will you do today?");	
 			Scanner userInput = new Scanner(System.in);
-			int numberQ = userInput.nextInt();
-			for(int i=0;i<numberQ;i++)
+			numberQ = userInput.nextInt();
+			for(int i=0;i<numberQ-2;i++)
 				{
 				generator();	
 				}
-			System.out.println("Your quiz is now complete, you got "+counter+" answers correct");
+			generatorEqual();
+			generatorLargerRight();
+			System.out.println("Your quiz is now complete, you got "+answerCounter+" answers correct");
 			}
 		public static int generator()
-			{
-				
-			int x = ((int)(Math.random()*24)+1);
+			{	
+			int x = ((int)(Math.random()*24)+12);
 			int y = ((int)(Math.random()*12)+1);
 			System.out.println("What is "+x+"%"+y+"?");
 			Scanner userInput = new Scanner(System.in);
@@ -25,12 +29,49 @@ public class Quizzer
 			if(userAnswer == answer )
 				{
 				System.out.println("Correct!");
-				counter++;
+				answerCounter++;
 				}
 			else
 				{
 				System.out.println("Sorry, incorrect");	
 				}
-			return counter;
+			return answerCounter;
+			}
+		public static int generatorEqual()
+			{
+			int x = ((int)(Math.random()*24)+1);
+			System.out.println("What is "+x+"%"+x+"?");
+			Scanner userInput = new Scanner(System.in);
+			int userAnswer = userInput.nextInt();
+			int answer = x%x;
+			if(userAnswer == answer )
+				{
+				System.out.println("Correct!");
+				answerCounter++;
+				}
+			else
+				{
+				System.out.println("Sorry, incorrect");	
+				}
+			return answerCounter;	
+			}
+		public static int generatorLargerRight()
+			{
+				int x = ((int)(Math.random()*12)+1);
+				int y = ((int)(Math.random()*24)+12);
+				System.out.println("What is "+x+"%"+y+"?");
+				Scanner userInput = new Scanner(System.in);
+				int userAnswer = userInput.nextInt();
+				int answer = x%y;
+				if(userAnswer == answer )
+					{
+					System.out.println("Correct!");
+					answerCounter++;
+					}
+				else
+					{
+					System.out.println("Sorry, incorrect");	
+					}
+			return answerCounter;	
 			}
 	}
